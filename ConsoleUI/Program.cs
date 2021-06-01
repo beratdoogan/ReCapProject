3b3e6af70951bd9d4fd0f2936709069c1821c6f4
Berat Doğan <77922331+beratdoogan@users.noreply.github.com>
2021-06-02 00:25:58 +0300

    git init - added ReCapProject

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Concrete;
+using Business.Abstract;
+using DataAccess.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            InMemoryCarDal ınMemoryCarDal = new InMemoryCarDal(new InMemoryCarDal());
+            ınMemoryCarDal.Add(new Car {Id=6,BrandId=2,ColorId=4,DailyPrice=100,ModelYear=2019,Description="Toyota"});
+
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine(item.Id + " " + item.Description);
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal
+    {
+        void Add(Car car);
+        void Delete(Car car);
+        void Update(Car car);
+        List<Car> GetAll();
+        List<Car> GetById(int ıd);
+    }
+}
