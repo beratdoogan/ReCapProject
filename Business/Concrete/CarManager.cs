3dca1b108408c62023bfd2defcf3404a19be703a
Berat Doğan <77922331+beratdoogan@users.noreply.github.com>
2021-06-20 13:04:52 +0300

    Add files via upload

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            _carDal.Add(c=> car.Description.Length<2 && car.DailyPrice>0);
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
+
+
+    }
+}
---
+++ Business/Concrete/ColorManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+
+namespace Business.Concrete
+{
+    public class ColorManager
+    {
+       
+    }
+}
